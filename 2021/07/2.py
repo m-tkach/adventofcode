d94cbb0620968926f3e01de49a833ec76d4b31bf
Michael Tkach <mtkach@fb.com>
2021-12-07 10:39:51 +0000

    Add day 7 (2021)

---
+++ 2021/07/2.py
@@ -0,0 +1,22 @@
+import sys
+from iparser import read_input
+
+def diff(data, x):
+    d = 0
+    for y in data:
+        n = abs(x - y)
+        d += n * (n + 1) // 2
+    return d
+
+def process(data):
+    data = sorted(data)
+    res = diff(data, data[-1])
+    for x in range(data[0], data[-1]):
+        res = min(res, diff(data, x))
+    return res
+
+if __name__ == '__main__':
+    input_file = 'example.txt' if '--example' in sys.argv[1:] else 'input.txt'
+    input_data = read_input(input_file)
+    result = process(input_data)
+    print(result)

b02333877bf0082d62267713e5bc6c48736c09d9
Michael Tkach <mtkach@fb.com>
2021-12-11 13:22:14 +0200

    Add day 11 (2021)

---
+++ 2021/11/1.py
@@ -0,0 +1,50 @@
+import sys
+from iparser import read_input
+
+class Queue:
+    def __init__(self):
+        self.data = []
+        self.front = 0
+
+    def empty(self):
+        return self.front >= len(self.data)
+
+    def push(self, x):
+        self.data.append(x)
+
+    def pop(self):
+        self.front += 1
+        return self.data[self.front - 1]
+
+STEPS = 100
+
+def process(data):
+    h, w = len(data), len(data[0])
+    flashes = 0
+    for _ in range(STEPS):
+        q = Queue()
+        for i in range(h):
+            for j in range(w):
+                data[i][j] += 1
+                if data[i][j] == 10:
+                    q.push((i, j))
+        while not q.empty():
+            i, j = q.pop()
+            flashes += 1
+            for di in -1, 0, 1:
+                for dj in -1, 0, 1:
+                    if not di == 0 == dj and 0 <= i + di < h and 0 <= j + dj < w:
+                        data[i + di][j + dj] += 1
+                        if data[i + di][j + dj] == 10:
+                            q.push((i + di, j + dj))
+        for i in range(h):
+            for j in range(w):
+                if data[i][j] > 9:
+                    data[i][j] = 0
+    return flashes
+
+if __name__ == '__main__':
+    input_file = 'example.txt' if '--example' in sys.argv[1:] else 'input.txt'
+    input_data = read_input(input_file)
+    result = process(input_data)
+    print(result)

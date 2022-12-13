ab1d2f7447174b4e3988123d0f2f2cf6ba7ea92b
Michael Tkach <mtkach@fb.com>
2022-12-13 13:47:06 +0000

    Remove sorting for packets in 2022/13/2

--- 2022/13/2.py
+++ 2022/13/2.py
@@ -17,32 +17,18 @@ def compare(left, right):
         i += 1
     return (i == len(right)) - (i == len(left))
 
-def sort_packets(packets):
-    n = len(packets)
-    if n < 2:
-        return
-    left_pile, right_pile = packets[:n//2], packets[n//2:]
-    sort_packets(left_pile)
-    sort_packets(right_pile)
-    left_i = right_i = 0
-    for i in range(n):
-        if left_i >= len(left_pile) or right_i < len(right_pile) and compare(left_pile[left_i], right_pile[right_i]) != -1:
-            packets[i] = right_pile[right_i]
-            right_i += 1
-        else:
-            packets[i] = left_pile[left_i]
-            left_i += 1
-
 def process(data):
-    dividers = [[[2]], [[6]]]
-    packets = dividers[:]
+    DIVIDERS = [[[2]], [[6]]]
+    assert all(compare(l, r) == -1 for l, r in zip(DIVIDERS, DIVIDERS[1:])), 'Const dividers should be sorted'
+    packets = []
     for datum in data:
         packets.extend(datum)
-    sort_packets(packets)
     result = 1
-    for i, packet in enumerate(packets, 1):
-        if packet in dividers:
-            result *= i
+    for position, divider in enumerate(DIVIDERS, 1):
+        for packet in packets:
+            if compare(packet, divider) == -1:
+                position += 1
+        result *= position
     return result
 
 if __name__ == '__main__':
